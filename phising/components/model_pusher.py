a07fe6421d0ee53b1565c99112958b44d8a34510
Syed-mansoor <saeedmansoor56@gmail.com>
2024-10-04 19:41:00 +0530

    model pusher added

--- phising/components/model_pusher.py
+++ phising/components/model_pusher.py
@@ -15,7 +15,7 @@ class ModelPusher:
         :param model_evaluation_artifact: Output reference of data evaluation artifact stage
         :param model_pusher_config: Configuration for model pusher
         """
-        # self.s3 = SimpleStorageService()
+        self.s3 = SimpleStorageService()
         self.model_evaluation_artifact = model_evaluation_artifact
         self.model_pusher_config = model_pusher_config
         self.usvisa_estimator = USvisaEstimator(bucket_name=model_pusher_config.bucket_name,
@@ -37,13 +37,13 @@ class ModelPusher:
             self.usvisa_estimator.save_model(from_file=self.model_evaluation_artifact.trained_model_path)
 
 
-            # model_pusher_artifact = ModelPusherArtifact(bucket_name=self.model_pusher_config.bucket_name,
-            #                                             s3_model_path=self.model_pusher_config.s3_model_key_path)
+            model_pusher_artifact = ModelPusherArtifact(bucket_name=self.model_pusher_config.bucket_name,
+                                                        s3_model_path=self.model_pusher_config.s3_model_key_path)
 
             logging.info("Uploaded artifacts folder to s3 bucket")
-            # logging.info(f"Model pusher artifact: [{model_pusher_artifact}]")
+            logging.info(f"Model pusher artifact: [{model_pusher_artifact}]")
             logging.info("Exited initiate_model_pusher method of ModelTrainer class")
             
-            # return model_pusher_artifact
+            return model_pusher_artifact
         except Exception as e:
             raise PhishingException(e, sys) from e
