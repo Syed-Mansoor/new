0a69a7319e9455a63ee2b32f4bb69ecb598b9d1f
Syed-mansoor <saeedmansoor56@gmail.com>
2024-10-12 11:08:59 +0530

    logging updated

--- Diamond/logger/__init__.py
+++ Diamond/logger/__init__.py
@@ -1,23 +1,32 @@
 import os
 import sys
 import logging
+from datetime import datetime
 
+# Define the log format
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s]"
 
+# Define the log directory
 log_dir = 'logs'
 
-log_filepath = os.path.join(log_dir,'Diamond.log') 
-os.makedirs(log_dir,exist_ok=True)
+# Generate a timestamp for the log file name
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
+# Create the log file path with the timestamp
+log_filepath = os.path.join(log_dir, f'Diamond_{timestamp}.log')
 
+# Create the log directory if it doesn't exist
+os.makedirs(log_dir, exist_ok=True)
+
+# Configure the logging settings
 logging.basicConfig(
     level=logging.INFO,
     format=logging_str,
-
-    handlers= [
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
+    handlers=[
+        logging.FileHandler(log_filepath),   # Log to file
+        logging.StreamHandler(sys.stdout)    # Log to console
     ]
 )
 
+# Create a logger instance
 logger = logging.getLogger('Diamond')
